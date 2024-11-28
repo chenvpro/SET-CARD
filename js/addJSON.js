0641c151ebb83014a55a671edd7a3fb11e5a5bfe
Vincent CHEN <chenv.pro@gmail.com>
2024-11-28 14:30:53 +0100

    Fonctionnalité  - ajout et suppression de joueurs opérationnels

---
+++ js/addJSON.js
@@ -0,0 +1,5 @@
+for (let player of players) {
+    if (player.value) {
+        //ajouter les noms des joueurs dans le fichier player.json
+    }
+}
--- js/alert.js
+++ js/alert.js
@@ -1,16 +1,12 @@
-import "./reloadJS.js";
-
-const player1 = document.getElementById("1");
-const player2 = document.getElementById("2");
 const btnStartGame = document.querySelector(".btn-start-game");
+const players = document.getElementsByClassName(".input-player");
 
 btnStartGame.addEventListener("click", function () {
-    if (player1.value && player2.value) {
-        btnStartGame.style.cursor = "pointer";
-        btnStartGame.style.pointerEvents = "auto";
-        btnStartGame.style.opacity = "1";
-        btnStartGame.href = "gameStart.html";
-    } else {
-        alert("Veuillez rentrer au moins deux noms de joueurs pour commencer la partie");
+    for (let player of players) {
+        if (player.value) {
+            btnStartGame.href = "gameStart.html";
+        } else {
+            alert("Veuillez rentrer au moins deux noms de joueurs pour commencer la partie");
+        }
     }
 });
